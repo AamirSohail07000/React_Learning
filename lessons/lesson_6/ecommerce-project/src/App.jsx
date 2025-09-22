391bd10a0b34a79ea56d2d53ac61affb62b87aad
AamirSohail07000 <sohail.aamir07000@gmail.com>
2025-09-23 02:51:14 +0530

    Move orders and Tracking page to react

--- lessons/lesson_6/ecommerce-project/src/App.jsx
+++ lessons/lesson_6/ecommerce-project/src/App.jsx
@@ -1,7 +1,9 @@
 import {Routes, Route} from 'react-router';
 import { HomePage } from './pages/HomePage';
-import './App.css'
 import { CheckoutPage } from './pages/CheckoutPage';
+import { OrdersPage } from './pages/OrdersPage';
+import { TrackingPage } from './pages/TrackingPage';
+import './App.css'
 
 function App() {
   
@@ -10,6 +12,8 @@ function App() {
     <Routes>
       <Route index element={<HomePage />} />
       <Route path="/checkout" element={<CheckoutPage />} />
+      <Route path="/orders" element={<OrdersPage />} />
+      <Route path="/tracking" element={<TrackingPage />} />
       
     </Routes>
     
